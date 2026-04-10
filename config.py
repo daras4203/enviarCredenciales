018332983d2c308bab0a414635971319a2754446
Hernan Dario a <hasprilla@cesde.net>
2026-04-10 12:11:58 -0500

    Carreccion de algunos errores

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 ARCHIVO_USUARIOS = "data/usuarioCorreo.xlsx"
 
-CORREO_REMITENTE = "Aprendizdo2@gco.com.co"
-PASSWORD_CORREO = "Colombia.26**."
+CORREO_REMITENTE = ""
+PASSWORD_CORREO = ""
 
-SMTP_SERVER = "smtp.office365.com"
+SMTP_SERVER = ""
 SMTP_PORT = 587
-CIFRADO = "STARTTLS (TLS)"
+CIFRADO = ""
 
 ASUNTO_CORREO = "Credenciales de acceso"
